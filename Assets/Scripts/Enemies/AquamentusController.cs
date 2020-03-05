f7a9d0cdbc3202021b030b6b25fe59ab293e0f47
rachelbobango <54557567+rachelbobango@users.noreply.github.com>
2020-03-05 17:56:30 -0500

    boomerang is fully functional
    -fixed speed of boomerang

---
+++ Assets/Scripts/Enemies/AquamentusController.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AquamentusController : MonoBehaviour
+{
+    public float speed;
+    public float projectileSpeed;
+    public float buffer;
+
+    public float moveTime;
+    float timer;
+    public float attackTime;
+    float attackTimer;
+    public float animateTime;
+    float animateTimer;
+
+    Vector2 initialPos = new Vector2();
+
+    private int direction;
+    private System.Random rand = new System.Random();
+
+    Rigidbody2D rigidbody2d;
+    Animator animator;
+
+    Vector2 lookDirection = new Vector2(-1, 0);
+
+    public GameObject projectilePrefab;
+
+    bool launched;
+    
+    void Start()
+    {
+        timer = moveTime;
+        attackTimer = attackTime;
+        animateTimer = animateTime;
+        direction = -1;
+
+        launched = false;
+
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        initialPos = rigidbody2d.position;
+        animator.SetFloat("Look X", -1);
+        animator.SetFloat("Look Y", 0);
+    }
+    
+    void Update()
+    {
+        moveWithAI();
+        attackTimer -= Time.deltaTime;
+        if (attackTimer < 0)
+        {
+            Launch();
+            attackTimer = attackTime;
+        }
+        if (launched)
+        {
+            animateTimer -= Time.deltaTime;
+            if (animateTimer < 0)
+            {
+                animator.SetFloat("Look X", -1);
+                animator.SetFloat("Look Y", 0);
+                launched = false;
+                animateTimer = animateTime;
+            }
+        }
+    }
+
+    public void moveWithAI()
+    {
+        changeDirection();
+        Vector2 position = rigidbody2d.position;
+        switch (direction)
+        {
+            case -1:
+                position = moveLeft(position);
+                break;
+            case 1:
+                position = moveRight(position);
+                break;
+            default:
+                timer = 0;
+                break;
+        }
+        if(position.x > initialPos.x + buffer || position.x < initialPos.x - buffer)
+        {
+            direction = -1 * direction;
+        }
+        rigidbody2d.position = position;
+    }
+
+    void changeDirection()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            direction = rand.Next(-2, 2);
+            timer = moveTime;
+        }
+    }
+
+    Vector2 moveRight(Vector2 position)
+    {
+        position.x += speed * Time.deltaTime;
+        return position;
+    }
+
+    Vector2 moveLeft(Vector2 position)
+    {
+        position.x -= speed * Time.deltaTime;
+        return position;
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        LinkController controller = other.gameObject.GetComponent<LinkController>();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
+    }
+
+    void Launch()
+    {
+        launched = true;
+        animator.SetFloat("Look X", 0);
+        animator.SetFloat("Look Y", 1);
+        GameObject projectile = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.2f, Quaternion.identity);
+
+        BoomerangProjectileController boomerangProjectile = projectile.GetComponent<BoomerangProjectileController>();
+        boomerangProjectile.Launch(lookDirection, projectileSpeed);
+
+    }
+}
---
+++ Assets/Scripts/Enemies/BladeTrapController.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeTrapController : MonoBehaviour
+{
+    public float speed;
+    private int direction;
+    Rigidbody2D rigidbody2d;
+    //LinkController controller;
+    Vector2 linkPosition;
+    bool moving;
+    bool returning;
+    Vector2 initialPosition;
+    public GameObject linkController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moving = false;
+        returning = false;
+        linkController = GameObject.Find("Link");
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        initialPosition = rigidbody2d.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        linkPosition = linkController.transform.position;
+        Vector2 position = rigidbody2d.position;
+        position = checkPosition(position);
+        rigidbody2d.position = position;
+    }
+
+    Vector2 checkPosition(Vector2 position)
+    {
+        if ((int)linkPosition.y == (int)position.y && !moving)
+        {
+            if (linkPosition.x < position.x)
+            {
+                direction = -1;
+            }
+            else
+            {
+                direction = 1;
+            }
+            moving = true;
+        }
+        else if ((int)linkPosition.x == (int)position.x && !moving)
+        {
+            if (linkPosition.y < position.y)
+            {
+                direction = 2;
+            }
+            else
+            {
+                direction = -2;
+            }
+            moving = true;
+        }
+        else if(moving)
+        {
+            position = charge(position);
+        }
+
+        if ((int)position.x == (int)initialPosition.x && (int)position.y == (int)initialPosition.y && returning)
+        {
+            position.x = initialPosition.x;
+            position.y = initialPosition.y;
+            moving = false;
+            speed = speed * 4;
+            returning = false;
+        }
+        return position;
+    }
+
+    Vector2 charge(Vector2 position)
+    {
+        switch (direction)
+        {
+            case -2:
+                position = moveUp(position);
+                break;
+            case -1:
+                position = moveLeft(position);
+                break;
+            case 1:
+                position = moveRight(position);
+                break;
+            case 2:
+                position = moveDown(position);
+                break;
+        }
+        return position;
+    }
+
+    Vector2 moveUp(Vector2 position)
+    {
+        position.y += speed * Time.deltaTime;
+        return position;
+    }
+
+    Vector2 moveDown(Vector2 position)
+    {
+        position.y -= speed * Time.deltaTime;
+        return position;
+    }
+
+    Vector2 moveRight(Vector2 position)
+    {
+        position.x += speed * Time.deltaTime;
+        return position;
+    }
+
+    Vector2 moveLeft(Vector2 position)
+    {
+        position.x -= speed * Time.deltaTime;
+        return position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        LinkController controller = other.gameObject.GetComponent<LinkController>();
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
+        else
+        {
+            direction = -1 * direction;
+            speed = speed / 4;
+            returning = true;
+        }
+    }
+}
