93afcf549e6c1738ce6b035fbe16b2057cb45787
MZehala <msz19981@gmail.com>
2020-04-23 15:16:20 -0400

    Triger blocks added

---
+++ Assets/Scripts/World/FloorTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorTrigger : MonoBehaviour
+{
+    public AudioClip secret;
+    AudioSource audioSource;
+    public GameObject door;
+    DoorSpriteController controller;
+
+    void Start()
+    {
+        controller = door.GetComponent<DoorSpriteController>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name.Contains("Block"))
+        {
+            controller.trigger = true;
+            PlaySound(secret);
+        }
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+}
